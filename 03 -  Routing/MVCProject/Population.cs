c8ed1c5a8cb9b2fe80324fe8efff01cc1dabae30
Rati <rati.shoshikelashvili@gmail.com>
2023-03-13 14:10:27 +0400

    add week 3 project

---
+++ 03 -  Routing/MVCProject/Population.cs
@@ -0,0 +1,142 @@
+﻿using Microsoft.AspNetCore.Authentication;
+
+namespace MVCProject
+{
+    public class Population
+    {
+        private RequestDelegate next;
+
+        public Population()
+        {
+        }
+        public Population(RequestDelegate nextDelegate)
+        {
+            next = nextDelegate;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            var parts = context.Request.Path.ToString().Split("/", StringSplitOptions.RemoveEmptyEntries);
+            if(parts.Length == 2 && parts[0] == "population")
+            {
+                var city = parts[1];
+                string population = default;
+
+                switch(city.ToLower())
+                {
+                    case "london":
+                        population = "8 Million";
+                        break;
+                    case "tbilisi":
+                        population = "4 Million";
+                        break;
+                    case "monaco":
+                        population = "40 Thousand";
+                        break;
+                }
+                if (!string.IsNullOrWhiteSpace(population))
+                {
+                    await context.Response.WriteAsync($"<h1>City: {city}, Population: {population}</h1>");
+                    return;
+                }
+            }
+            if (context is not null)
+                await next(context);
+        }
+
+        public static async Task Endpoint(HttpContext context)
+        {
+            string city = context.Request.RouteValues["city"] as string ?? "tbilisi";
+            string population = default;
+            switch((city ?? "").ToLower())
+            {
+                case "london":
+                    population = "8 Million";
+                    break;
+                case "tbilisi":
+                    population = "4 Million";
+                    break;
+                case "monaco":
+                    population = "40 Thousand";
+                    break;
+            }
+            if (!string.IsNullOrWhiteSpace(population))
+                await context.Response.WriteAsync($"<h1>City: {city}, Population: {population}</h1>");
+            else 
+                context.Response.StatusCode = StatusCodes.Status404NotFound;
+        }
+    }
+
+    public class Capital
+    {
+        private RequestDelegate next;
+
+        public Capital()
+        {
+        }
+
+        public Capital(RequestDelegate nextDelegate)
+        {
+            next = nextDelegate;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            var parts = context.Request.Path.ToString().Split("/", StringSplitOptions.RemoveEmptyEntries);
+            if (parts.Length == 2 && parts[0] == "capital")
+            {
+                var country = parts[1];
+                string capital = default;
+
+                switch (country.ToLower())
+                {
+                    case "uk":
+                        capital = "London";
+                        break;
+                    case "georgia":
+                        capital = "Tbilsi";
+                        break;
+                    //Example of dynamic URL generation
+                    case "monaco":
+                        var generator = context.RequestServices.GetService<LinkGenerator>();
+                        string url = generator?.GetPathByRouteValues(context, "population", new { city = country });
+                        if (!string.IsNullOrWhiteSpace(url))
+                            context.Response.Redirect(url);
+                        return;
+                }
+                if (!string.IsNullOrWhiteSpace(capital))
+                {
+                    await context.Response.WriteAsync($"<h1>{capital} is the capital of {country}</h1>");
+                    return;
+                }
+            }
+
+            if(context is not null)
+                await next(context);
+        }
+
+        //Refactored version of Capital class method
+        public static async Task Endpoint(HttpContext context)
+        {
+            string capital = default;
+            string country = context.Request.RouteValues["country"] as string;
+            switch((country ?? "").ToLower())
+            {
+                case "uk":
+                    capital = "London";
+                    break;
+                case "georgia":
+                    capital = "Tbilsi";
+                    break;
+                case "monaco":
+                    context.Response.Redirect($"/population/{country}");
+                    return;
+            }
+
+            if (!string.IsNullOrWhiteSpace(capital))
+                await context.Response.WriteAsync($"<h1>{capital} is the capital of {country}</h1>");
+            else
+                context.Response.StatusCode = StatusCodes.Status404NotFound;
+        }
+    }
+}
---
+++ 03 -  Routing/MVCProject/Program.cs
@@ -0,0 +1,119 @@
+using MVCProject;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.Configure<RouteOptions>(opts =>
+{
+    opts.ConstraintMap.Add("countryName", typeof(CustomRoutingConstraint));
+});
+
+var app = builder.Build();
+
+//Example of URL Routing with middlewares - 1
+app.UseMiddleware<Population>();
+app.UseMiddleware<Capital>();
+
+//Example of Built in Routing - 2
+app.UseRouting();
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapGet("routing", async context =>
+    {
+        await context.Response.WriteAsync("Request was routed");
+    });
+
+    //Example of Routing in combination with middlewares - 3
+    endpoints.MapGet("capital/uk", new Capital().Invoke);
+
+});
+
+
+//Example of directly using the mapping method, without the need to preconfigure other middlewares - 4
+//When defining routes directly, requests are always forwarded in the pipeline, so no terminal middleware should follow 
+app.MapGet("capital/uk", new Capital().Invoke);
+
+
+//Example of Segment variables - 5
+app.MapGet("{first}/{second}/{third}", async context =>
+{
+    await context.Response.WriteAsync($"Request was routed with segment variables\n");
+    foreach (var entry in context.Request.RouteValues)
+        await context.Response.WriteAsync($"{entry.Key} : {entry.Value}\n");
+});
+
+
+//Example of routing scoring system - 6
+app.MapGet("one/two/three", async context =>
+{
+    await context.Response.WriteAsync($"Request was routed with static values\n");
+    foreach (var entry in context.Request.RouteValues)
+        await context.Response.WriteAsync($"{entry.Key} : {entry.Value}\n");
+});
+
+//Example of refactored middleware routing - 7
+app.MapGet("capital/{country}", Capital.Endpoint);
+
+//Example of routing with Name metadata - 8
+app.MapGet("population/{city}", Population.Endpoint).WithMetadata(new RouteNameMetadata("population"));
+
+//Example of dynamic name routing in action - 9
+app.MapGet("size/{city}", Population.Endpoint).WithMetadata(new RouteNameMetadata("population"));
+
+//Example of routing pitfalls - 10
+//example/redgreen
+//example/redredgreen
+app.MapGet("example/red{color}", async context => { await context.Response.WriteAsync("Buggy route was matched!"); });
+
+//Example of default values - 11
+app.MapGet("capital/{country=Georgia}", Capital.Endpoint);
+
+//Example of optional value - 12
+app.MapGet("size/{city?}", Population.Endpoint).WithMetadata(new RouteNameMetadata("population"));
+
+//Example of catchall variable - 13
+app.MapGet("{first}/{second}/{*catchall}", async context =>
+{
+    await context.Response.WriteAsync($"Request was routed with catchall values\n");
+    foreach (var entry in context.Request.RouteValues)
+        await context.Response.WriteAsync($"{entry.Key} : {entry.Value}\n");
+});
+
+//Example of constrained route - 14
+app.MapGet("{first:int}/{second:bool}", async context =>
+{
+    await context.Response.WriteAsync($"Request was routed with constrained values\n");
+    foreach (var entry in context.Request.RouteValues)
+        await context.Response.WriteAsync($"{entry.Key} : {entry.Value}\n");
+});
+
+//Example of combined constraints - 15
+app.MapGet("{first:alpha:length(3)}/{second:bool}", async context =>
+{
+    await context.Response.WriteAsync($"Request was routed with combined constrained values\n");
+    foreach (var entry in context.Request.RouteValues)
+        await context.Response.WriteAsync($"{entry.Key} : {entry.Value}\n");
+});
+
+//Example of a fallback route - 16
+app.MapFallback(async context =>
+{
+    await context.Response.WriteAsync("Routed to fallback endpoint");
+});
+
+//Example of custom constraint - 17
+app.MapGet("capital/{country:countryName}", Capital.Endpoint);
+
+
+//Example of ambiguous endpoints - 18
+app.MapGet("{number:int}", async context =>
+{
+    await context.Response.WriteAsync("Routed to int endpoint");
+}).Add(b => ((RouteEndpointBuilder)b).Order = 1);
+
+app.MapGet("{number:double}", async context =>
+{
+    await context.Response.WriteAsync("Routed to double endpoint");
+}).Add(b => ((RouteEndpointBuilder)b).Order = 2); ;
+
+app.Run();
