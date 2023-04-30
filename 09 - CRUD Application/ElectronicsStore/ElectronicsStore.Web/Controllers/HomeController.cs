04f55b9b3493325b31e2c42978f475be8051ad10
Rati <rati.shoshikelashvili@gmail.com>
2023-04-30 13:51:09 +0400

    add initial frontent

---
+++ 09 - CRUD Application/ElectronicsStore/ElectronicsStore.Web/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using ElectronicsStore.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace ElectronicsStore.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewData["isHome"] = true;
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ 09 - CRUD Application/ElectronicsStore/ElectronicsStore.Web/Controllers/ProductsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ElectronicsStore.Web.Controllers
+{
+    public class ProductsController : Controller
+    {
+        public IActionResult List()
+        {
+            return View();
+        }
+    }
+}
